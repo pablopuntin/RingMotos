ded002d6cbd721dd4b001cc03b417b60eb358475
pablo putin <pablopuntin@gmail.com>
2025-12-13 16:31:49 -0300

    conexion a bd en render 9

--- src/common/typeorm.config.ts
+++ src/common/typeorm.config.ts
@@ -23,6 +23,9 @@
 //   }
 // });
 
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
 export const getTypeOrmConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
