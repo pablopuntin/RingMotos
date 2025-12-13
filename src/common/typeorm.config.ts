5a60b111ef99969c31503fb44fe5137f505b6713
pablo putin <pablopuntin@gmail.com>
2025-12-13 15:38:37 -0300

    conexion a bd en render 3

--- src/common/typeorm.config.ts
+++ src/common/typeorm.config.ts
@@ -1,74 +1,59 @@
-// // src/config/typeorm.config.ts
-// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-// import { ConfigService } from '@nestjs/config';
-
-// export const getTypeOrmConfig = (
-//   config: ConfigService
-// ): TypeOrmModuleOptions => {
-//   // Console.log para debuguear los .env
-//   console.log('DB_USERNAME:', config.get<string>('DB_USER'));
- 
-//   return {
-//     type: 'postgres',
-//     url: config.get<string>('DATABASE_URL') || undefined,
-//     host: config.get<string>('DB_HOST'),
-//     port: config.get<number>('DB_PORT') || 5432,
-//     username: config.get<string>('DB_USERNAME'),
-//     password: config.get<string>('DB_PASSWORD'),
-//     database: config.get<string>('DB_NAME'),
-//     ssl: config.get('DATABASE_URL') ? { rejectUnauthorized: false } : false,
-//     autoLoadEntities: true,
-//     synchronize: true, //true borra todo - DESHABILITADO DESPUÉS DE CREAR TABLA
-//     dropSchema: true // ⚠️ Nunca usar true en producción
-//   };
-// };
-
-// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-// import { ConfigService } from '@nestjs/config';
-
-// export const getTypeOrmConfig = (
-//   config: ConfigService
-// ): TypeOrmModuleOptions => {
-//   console.log('DATABASE_URL:', config.get<string>('DATABASE_URL'));
-
-//   return {
-//     type: 'postgres',
-//     url: config.get<string>('DATABASE_URL'),
-//     ssl: { rejectUnauthorized: false },
-//     autoLoadEntities: true,
-//     synchronize: true // ⚠️ usar migraciones en producción
-//   };
-// };
-
-
+// src/config/typeorm.config.ts
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
 export const getTypeOrmConfig = (
   config: ConfigService
 ): TypeOrmModuleOptions => {
-  const dbUrl = config.get<string>('DATABASE_URL');
-
-  if (dbUrl) {
-    // Render (producción)
-    return {
-      type: 'postgres',
-      url: dbUrl,
-      ssl: { rejectUnauthorized: false },
-      autoLoadEntities: true,
-      synchronize: true, // ⚠️ solo en desarrollo
-    };
-  }
-
-  // Local
+  // Console.log para debuguear los .env
+  console.log('DB_USERNAME:', config.get<string>('DB_USER'));
+ 
   return {
     type: 'postgres',
+    url: config.get<string>('DATABASE_URL') || undefined,
     host: config.get<string>('DB_HOST'),
-    port: parseInt(config.get<string>('DB_PORT'), 10),
-    username: config.get<string>('DB_USER'),
+    port: config.get<number>('DB_PORT') || 5432,
+    username: config.get<string>('DB_USERNAME'),
     password: config.get<string>('DB_PASSWORD'),
     database: config.get<string>('DB_NAME'),
+    ssl: config.get('DATABASE_URL') ? { rejectUnauthorized: false } : false,
     autoLoadEntities: true,
-    synchronize: true,
+    synchronize: true, //true borra todo - DESHABILITADO DESPUÉS DE CREAR TABLA
+    dropSchema: true // ⚠️ Nunca usar true en producción
   };
 };
+
+
+
+
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import { ConfigService } from '@nestjs/config';
+
+// export const getTypeOrmConfig = (
+//   config: ConfigService
+// ): TypeOrmModuleOptions => {
+//   const dbUrl = config.get<string>('DATABASE_URL');
+
+//   if (dbUrl) {
+//     // Render (producción)
+//     return {
+//       type: 'postgres',
+//       url: dbUrl,
+//       ssl: { rejectUnauthorized: false },
+//       autoLoadEntities: true,
+//       synchronize: true, // ⚠️ solo en desarrollo
+//     };
+//   }
+
+//   // Local
+//   return {
+//     type: 'postgres',
+//     host: config.get<string>('DB_HOST'),
+//     port: parseInt(config.get<string>('DB_PORT'), 10),
+//     username: config.get<string>('DB_USER'),
+//     password: config.get<string>('DB_PASSWORD'),
+//     database: config.get<string>('DB_NAME'),
+//     autoLoadEntities: true,
+//     synchronize: true,
+//   };
+// };
